ce94b220a48899ce784fe4a4df9947048dceec26
Federico Moretti <hello@federicomoretti.it>
2018-05-29 11:39:55 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Replace WebP with JPEG if unsupported by users’ browser
+ */
+if (!Modernizr.webp) {
+  const IMAGE = document.getElementsByClassName('site__sidebar__picture')[0];
+  let source = IMAGE.getAttribute('src');
+  source = source.replace(/webp/, 'jpg');
+  IMAGE.setAttribute('src', source);
+  let sourceSet = IMAGE.getAttribute('srcset');
+  sourceSet = sourceSet.replace(/webp/g, 'jpg');
+  IMAGE.setAttribute('srcset', sourceSet);
+}
