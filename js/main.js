11c7e7cd24c9f9c23d13024d0ebee0322f6214b0
Federico Moretti <hello@federicomoretti.it>
2018-05-30 10:39:48 +0000

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 /**
  * Replace WebP with JPEG if unsupported by users’ browser
  */
-if (!Modernizr.webp) {
-  const IMAGE = document.getElementsByClassName('site__sidebar__picture')[0];
-  let source = IMAGE.getAttribute('src');
-  source = source.replace(/webp/, 'jpg');
-  IMAGE.setAttribute('src', source);
-  let sourceSet = IMAGE.getAttribute('srcset');
-  sourceSet = sourceSet.replace(/webp/g, 'jpg');
-  IMAGE.setAttribute('srcset', sourceSet);
+Modernizr.on('webp', support => {
+  if (!support) {
+    const IMAGE = document.getElementsByClassName('site__sidebar__picture')[0];
+    let source = IMAGE.getAttribute('src');
+    source = source.replace(/webp/, 'jpg');
+    IMAGE.setAttribute('src', source);
+    let sourceSet = IMAGE.getAttribute('srcset');
+    sourceSet = sourceSet.replace(/webp/g, 'jpg');
+    IMAGE.setAttribute('srcset', sourceSet);
+  }
 }
