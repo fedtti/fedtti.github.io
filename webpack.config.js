720b4b8e26ff358157f5be7ffea58999341f8bad
Federico Moretti <hello@federicomoretti.it>
2021-02-25 14:42:01 +0100

    update dist/index.html and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.min.js',
+    filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
