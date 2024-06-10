02d679e1831f7b3f10fb5ef30c65e288c83c5f8b
Federico Moretti <hello@federicomoretti.it>
2024-06-10 12:16:52 +0200

    feat: add support for the /components folder in TypeScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { IBM_Plex_Sans } from 'next/font/google';
+import { IBM_Plex_Mono, IBM_Plex_Sans, IBM_Plex_Serif } from 'next/font/google';
 import "@/app/globals.css";
 
 const ibm_plex_sans = IBM_Plex_Sans({
