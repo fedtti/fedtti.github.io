15564f312f4393a370cdcc8e326b5b356fb91da0
Federico Moretti <hello@federicomoretti.it>
2024-06-15 13:10:20 +0200

    fix: update GitHub Actions configuration and the footer component

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -2,8 +2,7 @@
 
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleUp, faCircle } from '@fortawesome/free-solid-svg-icons';
-import { faBorderStyle } from '@fortawesome/free-solid-svg-icons/faBorderStyle';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
   const [currentYear, setCurrentYear] = useState<any>(getCurrentYear); // Show the current year.
@@ -22,7 +21,11 @@ export default function Footer() {
       <div className="row">
         <div className="col">
           <address>
-            
+            <ul>
+              <li><strong>Federico Moretti</strong><br />via San Martino della Battaglia, 9<br />21100 Varese (VA)</li>
+              <li>(+39) 351 819 2487</li>
+              <li>hello@federicomoretti.it</li>
+            </ul>
           </address>
         </div>
         <div className="col"></div>
@@ -31,7 +34,7 @@ export default function Footer() {
       </div>
       <div className="row">
         <div className="col">
-          <p><small>Federico Moretti © 2013-{currentYear}. All rights reserved.</small></p>
+          <p><small>Federico Moretti © 2013-{currentYear}. <wbr />All rights reserved.</small></p>
         </div>
         <div className="col">
           <ul>
