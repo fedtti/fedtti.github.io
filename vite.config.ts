39a77b88210e750f97da3425ea5465beeb6a627f
Federico Moretti <hello@federicomoretti.it>
2025-08-16 18:44:40 +0200

    fix: update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import type { UserConfig } from 'vite';
 
 export default {
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: './',
+    emptyOutDir: true,
+  },
 } satisfies UserConfig;
