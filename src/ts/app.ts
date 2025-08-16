1460a417f7e6967e7d2ffc1560086d03079b815a
Federico Moretti <hello@federicomoretti.it>
2025-08-16 15:24:44 +0200

    feat: add support for optional 'rtl' languages

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -9,43 +9,27 @@ const themeSwitcherIcon: HTMLElement | null = document.querySelector<HTMLElement
  * Load a saved theme from the local storage on page loading (fallback to system settings).
  */
 const loadTheme = (): void => {
-
   if (!!savedTheme) {
-
     document.documentElement.setAttribute('data-theme', savedTheme);
     themeSwitcherIcon!.setAttribute('title', `Switch to the ${savedTheme === 'light' ? 'dark' : 'light'} theme`);
-
     if (savedTheme === 'light') {
-
       themeSwitcherIcon!.classList.remove('fa-moon');
       themeSwitcherIcon!.classList.add('fa-sun');
-
     } else {
-
       themeSwitcherIcon!.classList.remove('fa-sun');
       themeSwitcherIcon!.classList.add('fa-moon');
-
     }
-
   } else if (!!systemTheme) {
-
     document.documentElement.setAttribute('data-theme', systemTheme);
     themeSwitcherIcon!.setAttribute('title', `Switch to the ${systemTheme === 'light' ? 'dark' : 'light'} theme`);
-
     if (systemTheme === 'light') {
-
       themeSwitcherIcon!.classList.remove('fa-moon');
       themeSwitcherIcon!.classList.add('fa-sun');
-
     } else {
-
       themeSwitcherIcon!.classList.remove('fa-sun');
       themeSwitcherIcon!.classList.add('fa-moon');
-
     }
-
   }
-
 };
 
 window.onload = loadTheme;
@@ -54,29 +38,21 @@ window.onload = loadTheme;
  * Switch the current theme between 'light' and 'dark' on user interaction.
  */
 const themeSwitch = (): void => {
-
   const currentTheme: string | null = document.documentElement.getAttribute('data-theme');
-
   if (currentTheme === 'light') {
-
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
     themeSwitcherIcon!.classList.remove('fa-sun');
     themeSwitcherIcon!.classList.add('fa-moon');
     themeSwitcherIcon!.setAttribute('title', 'Switch to the light theme');
-
   } else {
-
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
     themeSwitcherIcon!.classList.remove('fa-moon');
     themeSwitcherIcon!.classList.add('fa-sun');
     themeSwitcherIcon!.setAttribute('title', 'Switch to the dark theme');
-
   }
-
 };
 
 themeSwitcher!.addEventListener('click', themeSwitch, false);
-
 document.querySelector<HTMLSpanElement>('#current-year')!.innerText = new Date().getFullYear().toString(); // Show the current year.
