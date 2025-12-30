03ff37a80e66cc13961b1f381b72b64a346f1b1b
Federico Moretti <hello@federicomoretti.it>
2025-12-30 15:31:33 +0100

    feat: add 'Header' and 'Footer' core components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './core/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Header,
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
