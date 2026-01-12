6322a8d25d7609e2f241813897200b2b74fae75d
Federico Moretti <hello@federicomoretti.it>
2026-01-12 14:06:52 +0100

    chore: restore 'Theme' service name

--- src/app/shared/services/theme.ts
+++ src/app/shared/services/theme.ts
@@ -4,7 +4,7 @@ import { DOCUMENT } from '@angular/common';
 @Injectable({
   providedIn: 'root',
 })
-export class ThemeSwitch {
+export class Theme {
   constructor(@Inject(DOCUMENT) private readonly document: Document) {}
 
   private theme: 'system' | 'light' | 'dark' = 'system';
@@ -13,5 +13,8 @@ export class ThemeSwitch {
     return this.theme;
   }
 
+  setTheme(theme: 'system' | 'light' | 'dark'): void {
 
+    this.theme = theme;
+  }
 }
