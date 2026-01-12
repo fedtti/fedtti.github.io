d10cbbd16d755b20e538960c759cb4c056ff7b12
Federico Moretti <hello@federicomoretti.it>
2026-01-12 16:27:31 +0100

    chore: update 'ThemeService' to provide a theme

--- src/app/shared/components/header/theme/theme.ts
+++ src/app/shared/components/header/theme/theme.ts
@@ -1,4 +1,9 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  inject,
+  type Signal
+} from '@angular/core';
 import { ThemeService } from '../../../services/theme';
 
 @Component({
@@ -6,7 +11,11 @@ import { ThemeService } from '../../../services/theme';
   templateUrl: './theme.html',
   styleUrl: './theme.scss',
 })
-export class ThemeComponent {
-  protected readonly themeService = inject(ThemeService);
+export class ThemeComponent implements OnInit {
+  protected readonly themeService: ThemeService = inject(ThemeService);
+  protected readonly theme: Signal<'system' | 'light' | 'dark'> = this.themeService.getTheme();
 
+  ngOnInit(): void {
+    this.themeService.savedTheme();
+  }
 }
--- src/app/shared/services/theme.ts
+++ src/app/shared/services/theme.ts
@@ -1,4 +1,10 @@
-import { Injectable, Inject } from '@angular/core';
+import {
+  Injectable,
+  Inject,
+  signal,
+  type WritableSignal,
+  type Signal
+} from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Injectable({
@@ -6,15 +12,34 @@ import { DOCUMENT } from '@angular/common';
 })
 export class ThemeService {
   constructor(@Inject(DOCUMENT) private readonly document: Document) {}
+  private theme: WritableSignal<'system' | 'light' | 'dark'> = signal<'system' | 'light' | 'dark'>('system');
 
-  private theme: 'system' | 'light' | 'dark' = 'system';
-
-  getTheme(): 'system' | 'light' | 'dark' {
-    return this.theme;
+  getTheme(): Signal<'system' | 'light' | 'dark'> {
+    return this.theme.asReadonly();
   }
 
   setTheme(theme: 'system' | 'light' | 'dark'): void {
+    switch (theme) {
+      case 'system':
+        delete this.document.documentElement.dataset['theme'];
+        localStorage.removeItem('theme');
+        break;
+      case 'light':
+        this.document.documentElement.dataset['theme'] = 'light';
+        localStorage.setItem('theme', 'light');
+        break;
+      case 'dark':
+        this.document.documentElement.dataset['theme'] = 'dark';
+        localStorage.setItem('theme', 'dark');
+        break;
+    }
+    this.theme.set(theme);
+  }
 
-    this.theme = theme;
+  savedTheme(): void {
+    const savedTheme: 'system' | 'light' | 'dark' | null = localStorage.getItem('theme') as 'system' | 'light' | 'dark' || null;
+    if (!!savedTheme) {
+      this.setTheme(savedTheme);
+    }
   }
 }
