702d8152457ee1356698fd2574e86a3cfad41e56
Federico Moretti <hello@federicomoretti.it>
2026-08-24 19:40:31 +0200

    chore: get rid of useless style file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,4 @@
-import {
-  Component,
-  inject,
-  type Signal
-} from '@angular/core';
+import { Component, inject, type Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
 import { FooterComponent } from './shared/components/footer/footer';
@@ -10,13 +6,8 @@ import { ProfileService } from './shared/services/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
 export class App {
   protected readonly profileService: ProfileService = inject(ProfileService);
